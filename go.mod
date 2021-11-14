6686cdca46e958c3fb617a3dea7fc53cb986eb61
Zachary Duve <zacharyduve@gmail.com>
2021-11-13 21:53:45 -0500

    Should be working basic code, haha famous last words

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/ZacharyDuve/go-rasp-pi-spi-test
+
+go 1.16
+
+require (
+	periph.io/x/conn/v3 v3.6.9
+	periph.io/x/host/v3 v3.7.1
+)
---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"periph.io/x/conn/v3/physic"
+	"periph.io/x/conn/v3/spi"
+	"periph.io/x/conn/v3/spi/spireg"
+	"periph.io/x/host/v3"
+)
+
+const (
+	spiClockSpeed  physic.Frequency = physic.KiloHertz * 10
+	spiBusDevPath  string           = "/dev/spidev0"
+	spiDevPath     string           = spiBusDevPath + ".0"
+	spiMode        spi.Mode         = spi.Mode2
+	spiBitsPerWord int              = 8
+)
+
+func init() {
+	_, err := host.Init()
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	fmt.Println("Starting")
+	dataOut := []byte("Hello World!")
+	dataIn := make([]byte, len(dataOut), len(dataOut))
+	trxFunc, _, err := setupConnections(spiDevPath)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		time.Sleep(time.Second * 5)
+		fmt.Println("Writing message", dataOut)
+		trxFunc(dataOut, dataIn)
+	}
+}
+
+func setupConnections(spiDevPath string) (trxFunc func(w, r []byte) error, clsFunc func() error, err error) {
+	var initErr error
+
+	var spiPort spi.PortCloser
+	var spiConn spi.Conn
+
+	//Open port and connections
+	spiPort, initErr = spireg.Open(spiDevPath)
+	if initErr == nil {
+		spiConn, initErr = spiPort.Connect(spiClockSpeed, spiMode, spiBitsPerWord)
+
+		if initErr == nil {
+			clsFunc = func() error {
+				return spiPort.Close()
+			}
+			trxFunc = spiConn.Tx
+		}
+	}
+
+	return trxFunc, clsFunc, initErr
+}
